eaaedc180ebfdc149c85e896b764b1331e299fde
GURitch <guritch1@mail.ru>
2023-02-09 00:46:49 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+
+//        Я нем и рад я, так, трамвай, январь равняй, а в март катя, дари меня
+
+        String textChange = text.toLowerCase().replace(" ", "").replace(",","");
+        char[] symbols = textChange.toCharArray();
+
+        for (int i = 0; symbols.length / 2 > i; i++) {
+
+            if (symbols[i] != symbols[symbols.length - i - 1]) {
+                System.out.println("Строка '"+text+"' НЕ является палиндромом");
+                return;
+            }
+        }
+        System.out.println("Строка '"+text+"' является палиндромом");
     }
 }
